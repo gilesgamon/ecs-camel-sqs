122bb3c6caeaafdd31c3e21a7894f3bb2370fc2c
Giles Gamon <giles@lsiarchi.consulting>
2020-03-26 14:56:50 +0000

    small tidy-up

--- iac/ecs/main.tf
+++ iac/ecs/main.tf
@@ -35,22 +35,6 @@ module "ecs-service" {
 
 #For now we only use the AWS ECS optimized ami <https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html>
 
-data "aws_ami" "amazon_linux_ecs" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn-ami-*-amazon-ecs-optimized"]
-  }
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
-}
-
 data "aws_subnet_ids" "gifted_vpc" {
   vpc_id = var.vpc_id
 }
